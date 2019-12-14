d1dd4469d50bf6ad7be7799756cc7cc1a2b4c2bb
wuvin <wuvin@outlook.com>
2019-12-14 18:31:58 +0800

    Update dataset.py

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -11,13 +11,15 @@ from PIL import Image
 H, W = 500, 500
 FULL_CHANNEL = False
 
-tr3 = transforms.Normalize(mean = [0.5, 0.5, 0.5], std = [1, 1, 1])
-tr4 = transforms.Normalize(mean = [0.5, 0.5, 0.5, 0.5], std = [1, 1, 1, 1])
+tr3 = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[1, 1, 1])
+tr4 = transforms.Normalize(mean=[0.5, 0.5, 0.5, 0.5], std=[1, 1, 1, 1])
+
 
 class dataset(Dataset):
     def __init__(self, paths, label):
         self.img = paths
         self.label = label
+
     def __getitem__(self, idx):
         img = cv2.imread(self.img[idx], cv2.IMREAD_UNCHANGED if FULL_CHANNEL else cv2.IMREAD_COLOR)
         img = Image.fromarray(img)
@@ -26,13 +28,15 @@ class dataset(Dataset):
         img = tr4(img) if FULL_CHANNEL else tr3(img)
         label = self.label[idx]
         return img, label
+
     def __len__(self):
         return len(self.img)
+
 
 if __name__ == '__main__':
     paths, label = genconfig()
     dataset = dataset(paths, label)
-    dataloader = DataLoader(dataset, batch_size = 16, shuffle = True)
+    dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
     print('checking data...')
     for img, label in dataloader:
         pass
